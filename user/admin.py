9485b45508f7ff858d3d3e9fe79047355e6301d0
Yaroslav <yaroslav.liubarets@icloud.com>
2026-01-22 08:05:38 +0000

    Solution 2.2.1

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(User, UserAdmin)
+from user.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    # Fix the error: order by email instead of username
+    ordering = ("email",)
+    
+    # Show email in the list view instead of username
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    
+    # Configure the edit form to use email and remove username
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    
+    # Configure the "add user" form
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password", "first_name", "last_name", "is_staff", "is_superuser"),
+            },
+        ),
+    )
